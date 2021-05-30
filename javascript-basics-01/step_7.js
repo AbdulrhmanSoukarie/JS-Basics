06ee95b238dd94b861ba818926a73137fda16eda
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-30 16:42:53 +0300

    Step_7

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,12 @@
+
+
+function magic() {
+
+var shoe_size = document.getElementById('shoe_size').value;
+var year = document.getElementById('year').value;
+var result = ((shoe_size * 2) + 5) * 50  -  year  + 1766  ;
+
+
+alert ("The result : " + result) 
+ 
+}
