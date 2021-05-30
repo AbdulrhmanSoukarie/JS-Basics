55c869542426eb00565cc266bf0a31630ae785fe
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-30 17:14:05 +0300

    Step_8

---
+++ javascript-basics-01/step_8.js
@@ -0,0 +1,16 @@
+
+
+
+function ageUser() {
+ var user_age = document.getElementById('age').value;
+if (user_age <  18 ) {
+    alert("you are under 18" );
+}
+ else if (user_age > 18 ) {
+
+
+        alert("you are over 18") 
+    }
+
+       
+ } 
