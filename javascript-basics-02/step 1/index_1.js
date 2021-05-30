51d74951aa0417a72e69ad3453275cd4c45a9717
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-30 17:31:44 +0300

    Step_1

---
+++ javascript-basics-02/step 1/index_1.js
@@ -0,0 +1,13 @@
+var change_image = document.querySelector('#image1').addEventListener("mouseover" , imageChange) ;
+
+
+function imageChange() {
+
+    var image = document.getElementById("image1");
+    image.src = "images/image1_2.jpg" ;
+   
+
+    
+}
+
+
