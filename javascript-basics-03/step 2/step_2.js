379f4259adca15b0ec54f448bb89a5198db44c83
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-31 01:57:09 +0300

    Step_2

---
+++ javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,15 @@
+
+
+ 
+
+function myFunction(v) {
+    var x = document.getElementById("texte");
+    if (v === 0) {
+        x.style.display = "block";
+    } 
+    else {
+
+        x.style.display= "none"
+    }
+   
+}
