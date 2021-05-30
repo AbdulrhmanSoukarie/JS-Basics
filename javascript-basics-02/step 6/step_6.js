1ed86d8e68e03ae96363e08a8f6b5bbf6d36b49c
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-31 00:23:52 +0300

    step_6

---
+++ javascript-basics-02/step 6/step_6.js
@@ -0,0 +1,38 @@
+function change(v) {
+    if (v == 'image1_2') {
+        document.getElementById('image1').src = "images/image1_2.jpg"
+    }
+    else if  (v == 'image2_2' ){
+       document.getElementById('image2').src = "images/image2_2.jpg"
+    }
+    else if  (v == 'image3_2' ){
+       document.getElementById('image3').src = "images/image3_2.jpg"
+    }
+    else if  (v == 'image4_2' ){
+       document.getElementById('image4').src = "images/image4_2.jpg"
+    }
+    else if  (v == 'image5_2' ){
+       document.getElementById('image5').src = "images/image5_2.jpg"
+    }
+    
+  }
+function changeReset(v) {
+    if (v == 'image1_2') {
+        document.getElementById('image1').src = "images/image1.jpg"
+    }
+    else if  (v == 'image2_2' ){
+       document.getElementById('image2').src = "images/image2.jpg"
+    }
+    else if  (v == 'image3_2' ){
+       document.getElementById('image3').src = "images/image3.jpg"
+    }
+    else if  (v == 'image4_2' ){
+       document.getElementById('image4').src = "images/image4.jpg"
+    }
+    else if  (v == 'image5_2' ){
+       document.getElementById('image5').src = "images/image5.jpg"
+    }
+    
+  }
+
+  
