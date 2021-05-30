99baf249110fb6b8e79df494f069bd5a8556b835
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-30 13:56:45 +0300

    Step_5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,15 @@
+ window.onload = alert("please Enter two numbes");
+
+function multiply() {
+  
+    var firstNumber = document.getElementById('first_number').value ;
+    var secondNumber = document.getElementById('second_number').value;
+    
+    var result = firstNumber * secondNumber ;
+    
+    alert(result)
+}
+
+
+
+
