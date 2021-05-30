81b8738f9b02c33d339dff2db5aca9f29800386a
abdulrhman Soukarieh <abdulrhman-soukarieh@hotmail.com>
2021-05-31 00:05:09 +0300

    Step_5

--- javascript-basics-02/step 1/index_1.js
+++ javascript-basics-02/step 1/index_1.js
@@ -1,10 +1,14 @@
 var change_image = document.querySelector('#image1').addEventListener("mouseover" , imageChange) ;
 
+var image = document.getElementById("image1");
+
 
 function imageChange() {
 
-    var image = document.getElementById("image1");
+   
     image.src = "images/image1_2.jpg" ;
+   
+    
    
 
     
---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,21 @@
+
+
+
+function change(v) {
+  if (v == 'image1_2') {
+      document.getElementById('image1').src = "images/image1_2.jpg"
+  }
+  else if  (v == 'image2_2' ){
+     document.getElementById('image2').src = "images/image2_2.jpg"
+  }
+  else if  (v == 'image3_2' ){
+     document.getElementById('image3').src = "images/image3_2.jpg"
+  }
+  else if  (v == 'image4_2' ){
+     document.getElementById('image4').src = "images/image4_2.jpg"
+  }
+  else if  (v == 'image5_2' ){
+     document.getElementById('image5').src = "images/image5_2.jpg"
+  }
+  
+}
